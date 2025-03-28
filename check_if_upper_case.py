628801e27a580a55d5257f794e643327400f998c
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 23:19:49 +0800

    used swapcase and upper func in if else func to decide if entered charac is in uppercase

--- check_if_upper_case.py
+++ check_if_upper_case.py
@@ -1,5 +1,9 @@
-# Input a sentence
+# Input any characters
+user_char = str(input("Enter any characters: "))
 
-# Check sentence if upper-cased or not
+# Check sentence if upper-cased or not and print decision
+if user_char == user_char.swapcase().upper():
+    print("The inputted characters is in upper case.")
 
-# Print decision
+else:
+    print("The inputted characters is not in upper case.")
