d142f4fe46f88192b7b5224863de03c5e6b862f5
Vincent35p <vinsoyt4321@gmail.com>
2025-03-29 00:47:06 +0800

    used index for first letter and rest of the charac, then used upper and lower func then print the resultant

--- capitalize_string.py
+++ capitalize_string.py
@@ -1,5 +1,8 @@
 # Input any string characters
+user_char = str(input("Enter any string characters: "))
 
 # Capitalize the first letter, then lower case the rest
+capitalized_char = user_char[0].upper() + user_char[1:].lower()
 
 # Print the capitalized character
+print(capitalized_char)
