5fabd6a3a6d4cad5a65b42e4d5bbe2407ade16e0
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 23:56:12 +0800

    used format func to left align string and add spaces til specified parameter

--- l_justify_with_parameter.py
+++ l_justify_with_parameter.py
@@ -1,7 +1,11 @@
 # Input any characters
+user_char = str(input("Enter any character: "))
 
 # Input how many number of characters
+parameter = int(input("Enter the length of characters desired: "))
 
 # Add spaces to the inputted character to match the parameter given
+result = "{:<{}}".format(user_char, parameter)
 
 # Print the result
+print(result)
