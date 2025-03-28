b7868c5a7ef04eade2d23bb5a05a721601b121e4
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 22:38:55 +0800

    used .split func to remove the spaces, then .join func to join words back

--- remove_space_characters_without_l_strip.py
+++ remove_space_characters_without_l_strip.py
@@ -1,5 +1,8 @@
 # Input full name with spaces before the name
+user_name = str(input("Enter full name with several spaces before the name: "))
 
 # Remove spaces before the name
+modified_name = " ".join(user_name.split())
 
 # Print modified name
+print(modified_name)
