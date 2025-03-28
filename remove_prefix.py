997d592f2a5ca288b518b4ecfefa36c98aa03402
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 22:50:48 +0800

    used .lstrip func to set parameter on removing the prefix

--- remove_prefix.py
+++ remove_prefix.py
@@ -1,5 +1,8 @@
 # Initialize variable name
+name = "123Hello NewJeans."
 
 # Remove the certain character at the beginning of the string
+new_name = name.lstrip("123")
 
 # Print new name
+print(new_name)
