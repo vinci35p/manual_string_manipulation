de87d9dddda9e45e019199a3bd6ec38cd68ca626
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 23:01:57 +0800

    used for .upper func first then .swapcase func to swap all upper case to lower

--- lower_case.py
+++ lower_case.py
@@ -1,5 +1,9 @@
 # Input sentence
+sentence = str(input("Enter a sentence: "))
 
 # Convert all letters on sentence to lower case
+upper_cased_sentence = sentence.upper()
+modified_sentence = upper_cased_sentence.swapcase()
 
 # Print modified sentence
+print(modified_sentence)
