6ed8acc0e768e323c790a5dd1dec506eb132a2c7
Vincent35p <vinsoyt4321@gmail.com>
2025-03-29 01:01:04 +0800

    used split func to separate every word, then join to join back every word modified

--- string_title.py
+++ string_title.py
@@ -1,5 +1,9 @@
 # Input full name
+user_name = str(input("Enter your full name in random case: "))
 
 # Convert first letter of every word to upper case and the rest lower cased
+words = user_name.split()
+title_case = " ".join(word[0].upper() + word[1:].lower() if word else "" for word in words)
 
 # Print the result
+print(title_case)
