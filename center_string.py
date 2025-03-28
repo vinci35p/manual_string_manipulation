2e0983603cc1f9e6b6ad7b069eb3624b4c55a082
Vincent35p <vinsoyt4321@gmail.com>
2025-03-29 00:09:33 +0800

    created default parameter then the calculations

--- center_string.py
+++ center_string.py
@@ -1,7 +1,12 @@
 # Input any characters
+user_char = str(input("Enter any characters: "))
 
 # Input default parameter
+parameter = 50
 
 # Center the character based on the default parameter
+total_spaces = max(0, parameter - len(user_char))
+left = total_spaces // 2
+right = total_spaces - left
 
 # Print the result
