45171150a70323973ee4888836a56c3e4542db8d
Vincent35p <vinsoyt4321@gmail.com>
2025-03-29 00:39:05 +0800

    followed pep style coding standard edited some func

--- swapcase_string.py
+++ swapcase_string.py
@@ -4,8 +4,10 @@ user_char = str(input("Enter any string characters: "))
 # Swap the case of the characters
 swapped_characters = "".join(
     chr(ord(char) + 32) if 'A' <= char <= 'Z' else
-    chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-    for char in user_char)
+    chr(ord(char) - 32) if 'a' <= char <= 'z' else
+    char
+    for char in user_char
+)
 
 # Print the swapped characters
 print(swapped_characters)
