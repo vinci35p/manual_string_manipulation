31f38d06f89f24a4dcc2e6f75954c73762bd8d2f
Vincent35p <vinsoyt4321@gmail.com>
2025-03-28 23:39:28 +0800

    used string slicing by len func and list to extract the end word from sentence

--- check_if_end_string_match_parameter.py
+++ check_if_end_string_match_parameter.py
@@ -1,7 +1,12 @@
 # Input sentence
+sentence = str(input("Enter a sentence: "))
 
 # Input word to check if it matches the end of first input
+end_word = str(input("Input the ending word to check: "))
 
-# Decide if inputted value matches
+# Decide if inputted value matches and print decision
+if sentence[-len(end_word):] == end_word:
+    print("The sentence ends with the end word inputted!")
 
-# Print decision
+else:
+    print("The sentence does not end with the end word inputted.")
